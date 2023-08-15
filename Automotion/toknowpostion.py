497f89a021518bbae5851085742b63a224900ace
kelwinxd <kelwin_esechiel@hotmail.com>
2023-08-14 21:55:36 -0300

    up

--- Automotion/toknowpostion.py
+++ Automotion/toknowpostion.py
@@ -3,4 +3,14 @@ import pyautogui
 
 time.sleep(4)
 
-print(pyautogui.position())
+print("Partially", pyautogui.position())
+
+"""
+Yes Point(x=1541, y=341)
+No Point(x=1537, y=381)
+Partially Point(x=1558, y=414)
+Unclear Point(x=1538, y=449)
+Submit Point(x=1853, y=1000)
+
+
+"""
---
+++ moneytest/index.py
@@ -0,0 +1,14 @@
+import pyautogui as py
+import time
+
+py.PAUSE = 1.8
+#yes
+py.click(x=1541, y=341)
+py.click(x=1853, y=1000)
+
+time.sleep(5)
+
+py.click(x=1537, y=381)
+py.click(x=1853, y=1000)
+
+
