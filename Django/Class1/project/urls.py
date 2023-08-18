97c59dad12c1e29d8526c85d1e753c829e117f14
kelwinxd <kelwin_esechiel@hotmail.com>
2023-08-17 22:09:46 -0300

    up

--- Django/Class1/project/urls.py
+++ Django/Class1/project/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def my_view(request):
+    return HttpResponse('Kel Page')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('kel/', my_view)
 ]
